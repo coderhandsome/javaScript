15a6d3b473ffbfce6c2e70368750a64698d01c4d
coderhandsome <marsatonapa311@gmail.com>
2026-07-02 16:23:40 +0900

    Membuat file function.js dan mengisinya dengan function.

---
+++ function.js
@@ -0,0 +1,9 @@
+// Function mengonversi celsius ke fahrenheit
+function celciusToFahrenheit(celsius) {
+  const inFahrenheit = (9 / 5) * celsius + 32;
+  // Print
+  console.log("Hasil konversi:", inFahrenheit);
+}
+
+// Memanggil function
+celciusToFahrenheit(90);
