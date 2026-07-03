bcc708974d380d7407e082bce6922514df6ba8b9
coderhandsome <marsatonapa311@gmail.com>
2026-07-03 18:43:27 +0900

    Menambahkan function

---
+++ anotherFunction.js
@@ -0,0 +1,7 @@
+function convertCelsiusToFahrenheit(celsius) {
+    const inFahrenheit = 9/5 * celsius + 32;
+    console.log("Hasil konversi: " + inFahrenheit);
+
+}
+
+convertCelsiusToFahrenheit(45);
